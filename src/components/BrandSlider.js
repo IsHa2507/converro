32d040892b89c2d31377bb169a8cd58c23017ad7
ISHA VERMA <145704316+IsHa2507@users.noreply.github.com>
2025-08-28 22:48:55 +0530

    updates logos

--- src/components/BrandSlider.js
+++ src/components/BrandSlider.js
@@ -2,17 +2,22 @@ import React from "react";
 import "./BrandSlider.css";
 
 const logos = [
+  
   "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
   "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
-  "https://upload.wikimedia.org/wikipedia/commons/0/08/Google_Logo.svg",
+  "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg", // fixed Google
   "https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg",
   "https://upload.wikimedia.org/wikipedia/commons/5/51/IBM_logo.svg",
-"https://upload.wikimedia.org/wikipedia/commons/0/0e/Whatsapp_logo.svg",
+  "https://1000logos.net/wp-content/uploads/2021/04/WhatsApp-logo.png", // fixed WhatsApp
   "https://upload.wikimedia.org/wikipedia/commons/0/02/Stack_Overflow_logo.svg",
   "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
-  "https://upload.wikimedia.org/wikipedia/commons/2/2f/Instagram_logo_2016.svg",
-  "https://upload.wikimedia.org/wikipedia/commons/4/4f/Twitter-logo.svg"
+  "https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png",
+  "https://cdn-icons-png.flaticon.com/512/5968/5968756.png", // fixed X/Twitter
 ];
+
+
+
+
 
 const BrandSlider = () => {
   return (
