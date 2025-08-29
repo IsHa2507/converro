46c61a449669649a975bb8f4aae4332ae3686253
ISHA VERMA <145704316+IsHa2507@users.noreply.github.com>
2025-08-29 21:17:54 +0530

    updated aboutus

--- src/components/AboutValues.js
+++ src/components/AboutValues.js
@@ -1,30 +1,33 @@
 import React from "react";
 import { FaChartPie, FaChartLine, FaShieldAlt, FaCog } from "react-icons/fa";
 import "./AboutValues.css";
-
+import imgf1 from "../images/Customer.svg";
+import imgf2 from "../images/Excellence.svg";
+import imgf3 from "../images/Innovation1.svg";
+import imgf4 from "../images/Trust.svg";
 const features = [
   {
-    icon: <FaChartPie />,
+    image: imgf3,
     title: "Innovation at Core",
     text: " We harness AI and modern tech to build future-ready solutions.",
     color: "#0f5132",
   },
   {
-    icon: <FaChartLine />,
+    image: imgf1,
     title: "Customer First",
     text: "Every product and decision is centered on driving client growth.",
     color: "#f56539",
   },
   {
-    icon: <FaShieldAlt />,
+    image: imgf4,
     title: "Integrity & Transparency",
-    text: "We believe in honesty, clarity, and long-term trust.",
+    text: "We believe in honesty, clarity, and transparency to build lasting trust.",
     color: "#4f46e5",
   },
   {
-    icon: <FaCog />,
+    image: imgf2,
     title: "Excellence in Action ",
-    text: "We aim for simplicity, quality, and results that truly matter.",
+    text: "We aim for simplicity, quality, and real results that truly matter.",
     color: "#facc15",
   },
 ];
@@ -40,13 +43,15 @@ const Features = () => {
 
     <div className="features-section">
       {features.map((item, index) => (
-        <div className="feature-card" key={index}>
-          <div className="feature-icon" style={{ color: item.color }}>
-            {item.icon}
-          </div>
-          <h3 className="feature-title">{item.title}</h3>
-          <p>{item.text}</p>
-        </div>
+       <div className="feature-card" key={index}>
+  <div className="feature-icon">
+    <img src={item.image} alt={item.title} />
+  </div>
+  <div className="feature-content">
+    <h3>{item.title}</h3>
+    <p>{item.text}</p>
+  </div>
+</div>
       ))}
     </div>
   </div>
