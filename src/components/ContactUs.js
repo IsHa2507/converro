0b660e0cb1e48d06705d5b65e1060b20a640baf2
anmolSoni02 <anmol.soni@vitraya.com>
2025-09-05 00:20:44 +0530

    backend reverted

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -56,7 +56,7 @@ const ContactUs = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}`, {
+      const response = await fetch("http://127.0.0.1:8000/api/contact/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
