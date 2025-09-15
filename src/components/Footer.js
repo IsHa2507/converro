869c3707c78e2ae8b2050d315308d05e463b6a85
ISHA VERMA <145704316+IsHa2507@users.noreply.github.com>
2025-09-15 20:47:56 +0530

    Update Footer component with external links and improve structure for better accessibility

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,79 +12,99 @@ import {
 export default function Footer() {
   return (
     <footer className="footer">
-  <div className="footer-container">
-    {/* Brand */}
-    <div className="footer-brand">
-      <img src={logo} alt="Custo Logo" className="footer-logo" />
-      <p>
-        Simplify customer relationships and <br />
-        scale your business with Coverro.
-      </p>
-      <div className="social-icons">
-        <a href="#"><FaLinkedin /></a>
-        <a href="#"><FaFacebook /></a>
-        <a href="#"><FaInstagram /></a>
-        <a href="#"><FaYoutube /></a>
-      </div>
-    </div>
+      <div className="footer-container">
+        {/* Brand */}
+        <div className="footer-brand">
+          <img src={logo} alt="Custo Logo" className="footer-logo" />
+          <p>
+            Simplify customer relationships and <br />
+            scale your business with Coverro.
+          </p>
+          <div className="social-icons">
+            <a
+              href="https://www.linkedin.com/company/converro-io/posts/?feedView=all"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin />
+            </a>
+            <a
+              href="https://www.facebook.com/profile.php?id=61580426304606"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook />
+            </a>
+            <a
+              href="https://www.instagram.com/converro.io/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="https://www.youtube.com/@Converro"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaYoutube />
+            </a>
+          </div>
+        </div>
 
-    {/* Links */}
-    <div className="footer-links">
-      {/* <div>
-        <h3>Quick Links</h3>
-        <ul>
-          <li><a href="#">Whatsapp API</a></li>
-          <li><a href="#">Automations</a></li>
-          <li><a href="#">SMS Marketing</a></li>
-          <li><a href="#">IVR Solutions</a></li>
-          <li><a href="#">AI Bot Assistant</a></li>
-          <li><a href="#">Task Manager</a></li>
-          
-        </ul>
-      </div> */}
-      <div>
-        <h3>Main Pages</h3>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/aboutus">About</a></li>
-          <li><a href="/pricing">Pricing</a></li>
-          <li><a href="#">Products</a></li>
-          <li><a href="#">Industries</a></li>
-          {/* <li><a href="#">Blog</a></li> */}
-          <li><a href="/contactus">Contact Us</a></li>
-        </ul>
+        {/* Links */}
+        <div className="footer-links">
+          <div>
+            <h3>Quick Links</h3>
+            <ul>
+              <li><a href="#">Whatsapp API</a></li>
+              <li><a href="#">Automations</a></li>
+              <li><a href="#">SMS Marketing</a></li>
+              <li><a href="#">IVR Solutions</a></li>
+              <li><a href="#">AI Bot Assistant</a></li>
+              <li><a href="#">Task Manager</a></li>
+            </ul>
+          </div>
+          <div>
+            <h3>Main Pages</h3>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/aboutus">About</a></li>
+              <li><a href="/pricing">Pricing</a></li>
+              <li><a href="#">Products</a></li>
+              <li><a href="#">Industries</a></li>
+              {/* <li><a href="#">Blog</a></li> */}
+              <li><a href="/contactus">Contact Us</a></li>
+            </ul>
+          </div>
+          <div>
+            <h3>Important</h3>
+            <ul>
+              {/* <li><a href="#">FAQs</a></li> */}
+              <li><a href="/termsandconditions">Terms & Conditions</a></li>
+              <li><a href="/privacypolicy">Privacy Policy</a></li>
+              <li><a href="/refundpolicy">Refund Policy</a></li>
+              <li><a href="/contactus">Support</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div>
-        <h3>Important</h3>
-        <ul>
-          {/* <li><a href="#">FAQs</a></li> */}
-          <li><a href="/termsandconditions">Terms & Conditions</a></li>
-          <li><a href="/privacypolicy">Privacy Policy</a></li>
-          <li><a href="/refundpolicy">Refund Policy</a></li>
-          <li><a href="/contactus">Support</a></li>
-          
-        </ul>
+
+      {/* Bottom */}
+      <div className="footer-bottom">
+        <p>2025 © Converro a subsidiary of Cybershield Technologies Pvt. Ltd.</p>
+        <p>
+          <a
+            href="https://cybershieldtechnologies.com/"
+            className="footer-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Cybershield Technologies Pvt. Ltd.
+            <ArrowUpRight size={13} className="arrow-icon" />
+          </a>
+        </p>
       </div>
-    </div>
-  </div>
-
-  {/* Bottom */}
-  <div className="footer-bottom">
-  <p>2025 © Converro a subsidiary of Cybershield Technologies Pvt. Ltd.</p>
-  <p>
-   <a
-  href="https://cybershieldtechnologies.com/"
-  className="footer-link"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  Cybershield Technologies Pvt. Ltd.
-  <ArrowUpRight size={13} className="arrow-icon" />
-</a>
-  </p>
-</div>
-</footer>
-
+    </footer>
   );
 }
-
